3c23b1575702c9f325109fcf8710225416d584b3
eya-ouni <eyaouniessect@gmail.com>
2025-08-07 09:45:08 +0100

    final version

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-print("✅ GPU disponible :", torch.cuda.is_available())
-
-if torch.cuda.is_available():
-    print("Nom du GPU :", torch.cuda.get_device_name(0))
-else:
-    print("Aucun GPU détecté, exécution sur CPU.")
